bc6cb742c03d0ef2cc0477ac0c12810e2186a000
AmitGujar <amithero3342@gmail.com>
2023-04-01 14:38:07 +0530

    aks config added

--- AKS/variables.tf
+++ AKS/variables.tf
@@ -11,3 +11,39 @@ variable "resource_group_name_prefix" {
   default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
+
+variable "agent_count" {
+  default = 2
+}
+
+variable "aks_service_principal_app_id" {
+  default = ""
+}
+
+variable "aks_service_principal_client_secret" {
+  default = ""
+}
+
+variable "cluster_name" {
+  default = "k8stest"
+}
+
+variable "dns_prefix" {
+  default = "k8stest"
+}
+
+variable "log_analytics_workspace_location" {
+  default = "centralindia"
+}
+
+variable "log_analytics_workspace_name" {
+  default = "testLogAnalyticsWorkspaceName"
+}
+
+variable "log_analytics_workspace_sku" {
+  default = "PerGB2018"
+}
+
+variable "ssh_public_key" {
+  default = "~/.ssh/id_rsa.pub"
+}
